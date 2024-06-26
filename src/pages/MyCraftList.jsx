7a3e974e435011695315f000805527a0b01f9d9a
roufrubel <rubel.ma2012@gmail.com>
2024-06-27 00:29:55 +0600

    my craft list added to table format

--- src/pages/MyCraftList.jsx
+++ src/pages/MyCraftList.jsx
@@ -7,28 +7,40 @@ const MyCraftList = () => {
     const crafts = useLoaderData();
     const {user} = useContext(AuthContext);
     const myCrafts = crafts.filter(craft => craft.user_email === user.email);
-    const {_id} = crafts;
     return (
         <div>
             <h2>My Craft List</h2>
             <div>
                 {
                     myCrafts.length > 0 ? (
-                        <ul>
-                          {myCrafts.map((craft) => (
-                            <li key={_id}>
-                              <h2>{craft.item_name}</h2>
-                              {/* <p>{craft.short_description}</p>
-                              <img src={craft.image} alt={craft.item_name} />
-                              <p>Price: ${craft.price}</p>
-                              <p>Rating: {craft.rating}</p>
-                              <p>Customization: {craft.customization}</p>
-                              <p>Processing Time: {craft.processing_time}</p>
-                              <p>Stock Status: {craft.stockStatus}</p>
-                              <p>Seller: {craft.user_name} ({craft.user_email})</p> */}
-                            </li>
+                        <div>
+                          {myCrafts.map((craft, idx) => (
+                            // <li >
+
+<div className="overflow-x-auto" key={idx}>
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+        <th></th>
+        <th>Craft Name</th>
+        <th>Price</th>
+        <th>Stock Status</th>
+      </tr>
+    </thead>
+    <tbody>
+      {/* row 1 */}
+      <tr>
+        <th>{idx}</th>
+        <td>{craft.item_name}</td>
+        <td>${craft.price}</td>
+        <td>{craft.stockStatus}</td>
+      </tr>     
+    </tbody>
+  </table>
+</div>
                           ))}
-                        </ul>
+                        </div>
                       ) : (
                         <p>No crafts available</p>
                       )
@@ -39,3 +51,18 @@ const MyCraftList = () => {
 };
 
 export default MyCraftList;
+
+
+
+
+
+{/* <h2>{craft.item_name}</h2>
+                              <p>{craft.short_description}</p>
+                              <img src={craft.image} alt={craft.item_name} />
+                              <p>Price: ${craft.price}</p>
+                              <p>Rating: {craft.rating}</p>
+                              <p>Customization: {craft.customization}</p>
+                              <p>Processing Time: {craft.processing_time}</p>
+                              <p>Stock Status: {craft.stockStatus}</p>
+                              <p>Seller: {craft.user_name} ({craft.user_email})</p>  */}
+                            
